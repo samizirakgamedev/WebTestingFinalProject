e4a4efcc0c31469de06f4325b8a1caece2c74d6c
Autumn Pelešová <autumnlara@hotmail.com>
2022-03-07 14:00:38 +0000

    Removed parts which I will be adding to interface

--- src/test/java/org/carefulchameleons/pom/category/WomenPage.java
+++ src/test/java/org/carefulchameleons/pom/category/WomenPage.java
@@ -10,22 +10,10 @@ import java.util.List;
 public class WomenPage extends Page {
     private WebDriver driver;
     private String url = "http://automationpractice.com/index.php?id_category=3&controller=category";
-    protected final By BY_PRODUCT_CONTAINER = new By.ByClassName("product-container");
-    protected final By BY_PRODUCT_NAME = new By.ByClassName("product-name");
+
 
     public WomenPage(WebDriver driver) {
         this.driver = driver;
     }
 
-    public boolean areProductsInOrder(List<String> expectedOrder) {
-
-    }
-
-    private List<WebElement> getProducts() {
-        return driver.findElements(BY_PRODUCT_CONTAINER);
-    }
-
-    private String getProductName(WebElement w) {
-        return w.findElement(BY_PRODUCT_NAME).getText();
-    }
 }
