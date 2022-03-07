88965db6ff3aea4354644523dba4a4969d4532ef
Gideon9876543 <98388179+Gideon9876543@users.noreply.github.com>
2022-03-07 15:17:10 +0000

    T&C Page Updated
    terms and conditions page has been given webdriver.

--- src/test/java/org/carefulchameleons/pom/information/TermsAndConditionsPage.java
+++ src/test/java/org/carefulchameleons/pom/information/TermsAndConditionsPage.java
@@ -1,4 +1,15 @@
 package org.carefulchameleons.pom.information;
 
-public class TermsAndConditionsPage {
+import org.carefulchameleons.pom.Page;
+import org.openqa.selenium.WebDriver;
+
+public class TermsAndConditionsPage extends Page {
+
+    public TermsAndConditionsPage(WebDriver webDriver) {
+        super(webDriver, "http://automationpractice.com/index.php?id_cms=3&controller=cms");
+    }
+
+    public String getCurrentURL() {
+        return getWebDriver().getCurrentUrl();
+    }
 }
