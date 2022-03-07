03f66b65c452fe5d25d7b45a268cc314a9545998
AntonyA3 <41267884+AntonyA3@users.noreply.github.com>
2022-03-07 17:48:26 +0000

    I have created some more page elements and made changes to NewProductsPage

--- src/test/java/org/carefulchameleons/pageelements/LogoButton.java
+++ src/test/java/org/carefulchameleons/pageelements/LogoButton.java
@@ -1,2 +1,22 @@
-package org.carefulchameleons.pageelements;public class LogoButton {
+package org.carefulchameleons.pageelements;
+
+import org.carefulchameleons.pom.IndexPage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class LogoButton {
+    private WebDriver webDriver;
+    private By logoButton;
+    public LogoButton(WebDriver webDriver){
+        this.webDriver = webDriver;
+        logoButton = By.className("logo");
+    }
+
+    public IndexPage click(){
+        webDriver.findElement(logoButton).click();
+        return new IndexPage(webDriver);
+    }
+
+
+
 }
--- src/test/java/org/carefulchameleons/pageelements/SearchBar.java
+++ src/test/java/org/carefulchameleons/pageelements/SearchBar.java
@@ -1,2 +1,36 @@
-package org.carefulchameleons.pageelements;public class SearchBar {
+package org.carefulchameleons.pageelements;
+
+import org.carefulchameleons.pom.SearchPage;
+import org.carefulchameleons.pom.information.NewProductsPage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class SearchBar {
+    private WebDriver webDriver;
+    private By searchBar;
+    private By searchButton;
+
+
+    public SearchBar(WebDriver webDriver){
+        this.webDriver = webDriver;
+        searchBar = By.name("search_query");
+        searchButton = By.name("submit_search");
+    }
+
+
+    public SearchPage doSearch(String entry){
+        webDriver.findElement(searchBar).sendKeys(entry);
+        webDriver.findElement(searchButton).click();
+        return new SearchPage(webDriver);
+    }
+
+    public SearchBar assignSearchBarInput(By inputBox){
+        searchBar = inputBox;
+        return this;
+    }
+
+    public SearchBar assignSearchBarButton(By button){
+        searchButton = button;
+        return this;
+    }
 }
