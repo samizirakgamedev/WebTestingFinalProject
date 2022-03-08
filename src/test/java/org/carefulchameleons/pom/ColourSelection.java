a249653d4312047108eca6b87080cb9057a63ac6
Gideon9876543 <98388179+Gideon9876543@users.noreply.github.com>
2022-03-08 11:36:40 +0000

    CategoryPageSelection Updated
    Enum updated to give the id of each colour and category page update to give methods that click each possible checkbox.

--- src/test/java/org/carefulchameleons/pom/ColourSelection.java
+++ src/test/java/org/carefulchameleons/pom/ColourSelection.java
@@ -1,21 +1,44 @@
 package org.carefulchameleons.pom;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 
 public enum ColourSelection {
 
-    GREEN("green"),
-    YELLOW("yellow"),
-    BLUE("blue"),
-    ORANGE("orange"),
-    BEIGE("beige"),
-    WHITE("white"),
-    PINK("pink"),
-    BLACK("black");
-    
-    public String NAME;
+//    GREEN("green"),
+//    YELLOW("yellow"),
+//    BLUE("blue"),
+//    ORANGE("orange"),
+//    BEIGE("beige"),
+//    WHITE("white"),
+//    PINK("pink"),
+//    BLACK("black");
+//
+//    public String NAME;
 
-    ColourSelection(String name) {
-        NAME = name;
+    GREEN("layered_id_attribute_group_15"),
+    YELLOW("layered_id_attribute_group_16"),
+    BLUE("layered_id_attribute_group_14"),
+    ORANGE("layered_id_attribute_group_13"),
+    BEIGE("layered_id_attribute_group_7"),
+    WHITE("layered_id_attribute_group_8"),
+    PINK("layered_id_attribute_group_24"),
+    BLACK("layered_id_attribute_group_11");
+
+    public String ID;
+
+    ColourSelection(String id) {
+        ID = id;
+    }
+
+    public void clickCheckbox(WebDriver webDriver){
+        webDriver.findElement(new By.ById(GREEN.ID)).click();
+        webDriver.findElement(new By.ById(YELLOW.ID)).click();
+        webDriver.findElement(new By.ById(BLUE.ID)).click();
+        webDriver.findElement(new By.ById(ORANGE.ID)).click();
+        webDriver.findElement(new By.ById(BEIGE.ID)).click();
+        webDriver.findElement(new By.ById(WHITE.ID)).click();
+        webDriver.findElement(new By.ById(PINK.ID)).click();
+        webDriver.findElement(new By.ById(BLACK.ID)).click();
     }
 }
--- src/test/java/org/carefulchameleons/pom/category/CategoryPage.java
+++ src/test/java/org/carefulchameleons/pom/category/CategoryPage.java
@@ -1,7 +1,9 @@
 package org.carefulchameleons.pom.category;
 
+import org.carefulchameleons.pom.ColourSelection;
 import org.carefulchameleons.pom.Page;
 import org.carefulchameleons.pom.ProductSelection;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class CategoryPage extends Page {
@@ -18,4 +20,115 @@ public class CategoryPage extends Page {
     public ProductSelection getProductSelection() {
         return new ProductSelection(webDriver, this);
     }
+
+    public CategoryPage selectComposition(String composition){
+        switch(composition) {
+            case "cotton":
+                webDriver.findElement(new By.ById("layered_id_feature_5")).click();
+                break;
+            case "polyester":
+                webDriver.findElement(new By.ById("layered_id_feature_1")).click();
+                break;
+            case "viscose":
+                webDriver.findElement(new By.ById("layered_id_feature_3")).click();
+                break;
+        }
+        return this;
+    }
+
+    public CategoryPage selectStyle(String styles){
+        switch(styles){
+            case "casual":
+                webDriver.findElement(new By.ById("layered_id_feature_11")).click();
+                break;
+            case "dressy":
+                webDriver.findElement(new By.ByName("layered_id_feature_16")).click();
+                break;
+            case "girly":
+                webDriver.findElement(new By.ByName("layered_id_feature_13")).click();
+                break;
+        }
+        return this;
+    }
+
+    public CategoryPage selectSize(String size){
+        switch(size){
+            case "s":
+                webDriver.findElement(new By.ByName("layered_id_attribute_group_1")).click();
+                break;
+            case "m":
+                webDriver.findElement(new By.ByName("layered_id_attribute_group_2")).click();
+                break;
+            case "l":
+                webDriver.findElement(new By.ByName("layered_id_attribute_group_3")).click();
+                break;
+        }
+        return this;
+    }
+
+    public CategoryPage selectCategory(String category) {
+        switch (category) {
+            case "tops":
+                webDriver.findElement(new By.ByName("layered_category_4")).click();
+                break;
+            case "dresses":
+                webDriver.findElement(new By.ByName("layered_category_8")).click();
+                break;
+        }
+        return this;
+    }
+
+    public CategoryPage selectProperties(String property){
+        switch(property){
+            case "colourful dress":
+                webDriver.findElement(new By.ByName("layered_id_feature_18")).click();
+                break;
+            case "midi dress":
+                webDriver.findElement(new By.ByName("layered_id_feature_20")).click();
+                break;
+            case "short sleeve":
+                webDriver.findElement(new By.ByName("layered_id_feature_17")).click();
+                break;
+            case "maxi dress":
+                webDriver.findElement(new By.ByName("layered_id_feature_21")).click();
+                break;
+            case "short dress":
+                webDriver.findElement(new By.ByName("layered_id_feature_19")).click();
+                break;
+        }
+        return this;
+    }
+
+    public CategoryPage selectAvailability(String availability) {
+        switch (availability) {
+            case "in stock":
+                webDriver.findElement(new By.ByName("layered_quantity_1")).click();
+                break;
+        }
+        return this;
+    }
+
+    public CategoryPage selectManufacturer(String manufacturer) {
+        switch (manufacturer) {
+            case "fashion manufacturer":
+                webDriver.findElement(new By.ByName("layered_manufacturer_1")).click();
+                break;
+        }
+        return this;
+    }
+
+    public CategoryPage selectCondition(String condition) {
+        switch (condition) {
+            case "new":
+                webDriver.findElement(new By.ByName("layered_condition_new")).click();
+                break;
+        }
+        return this;
+    }
+
+    public CategoryPage selectColour(ColourSelection colour) {
+        colour.clickCheckbox(webDriver);
+        return this;
+    }
+
 }
