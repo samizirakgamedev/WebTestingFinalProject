09ca92498f4815be8e2579dd92e358e6f2388501
DalePBernardo <dalepb18@gmail.com>
2022-03-08 15:20:47 +0000

    An attempt to fix the dev branch

--- src/test/java/org/carefulchameleons/pom/cart/CartSummaryPage.java
+++
@@ -1,9 +0,0 @@
-package org.carefulchameleons.pom.cart;
-
-import org.openqa.selenium.WebDriver;
-
-public class CartSummaryPage extends CartPage {
-    public CartSummaryPage(WebDriver webDriver) {
-        super(webDriver, "");
-    }
-}
