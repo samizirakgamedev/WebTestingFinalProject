7d5259591097513c8c85f07fd3fa66a6b9c67fa8
Sami Z <79335705+samizirakgamedev@users.noreply.github.com>
2022-03-09 17:06:02 +0000

    Update WebDriverTest.java
    Made the methods for testing the drivers more modular and easy to use. May be converted into JUnit tests later.

--- src/test/java/org/carefulchameleons/webdrivers/WebDriverTest.java
+++ src/test/java/org/carefulchameleons/webdrivers/WebDriverTest.java
@@ -1,86 +1,57 @@
 package org.carefulchameleons.webdrivers;
 
+import org.carefulchameleons.webdrivers.model.MobilePhoneType;
 import org.openqa.selenium.WebDriver;
 import org.carefulchameleons.webdrivers.model.WebDriverManager;
 import org.carefulchameleons.webdrivers.model.WebDriverType;
 
 public class WebDriverTest {
 
-    public static void chromeTest(){
+    public static void testDriver(WebDriverType webDriverType){
         WebDriverManager driverManager;
         WebDriver driver;
         String desiredPage = "https://www.google.com/";
 
-        driverManager = WebDriverFactory.getManager(WebDriverType.CHROME);
-        driver = driverManager.getIPhoneElevenDriver();
+        driverManager = WebDriverFactory.getManager(webDriverType);
+        driver = driverManager.getDriver();
         driver.get(desiredPage);
 
         if (driver.getCurrentUrl().equals(desiredPage))
-            System.out.println(">>> Chrome Driver Functioning <<<");
+            System.out.println(">>> " + webDriverType + " Driver Functioning <<<");
         driverManager.quitDriver();
     }
 
-    public static void firefoxTest(){
+    public static void testHeadlessDriver(WebDriverType webDriverType){
         WebDriverManager driverManager;
         WebDriver driver;
         String desiredPage = "https://www.google.com/";
 
-        driverManager = WebDriverFactory.getManager(WebDriverType.FIREFOX);
-        driver = driverManager.getIPhoneElevenDriver();
+        driverManager = WebDriverFactory.getManager(webDriverType);
+        driver = driverManager.getHeadlessDriver();
         driver.get(desiredPage);
 
         if (driver.getCurrentUrl().equals(desiredPage))
-            System.out.println(">>> Firefox Driver Functioning <<<");
+            System.out.println(">>> " + webDriverType + " Driver Functioning <<<");
         driverManager.quitDriver();
     }
 
-    public static void ieTest(){
+    public static void testDriverMobile(WebDriverType webDriverType, MobilePhoneType mobilePhoneType){
         WebDriverManager driverManager;
         WebDriver driver;
         String desiredPage = "https://www.google.com/";
 
-        driverManager = WebDriverFactory.getManager(WebDriverType.IE);
-        driver = driverManager.getIPhoneElevenDriver();
+        driverManager = WebDriverFactory.getManager(webDriverType);
+        driver = driverManager.getMobilePhoneDriver(mobilePhoneType);
         driver.get(desiredPage);
 
         if (driver.getCurrentUrl().equals(desiredPage))
-            System.out.println(">>> IE Driver Functioning <<<");
-        //driverManager.quitDriver();
-    }
-
-    public static void operaTest(){
-        WebDriverManager driverManager;
-        WebDriver driver;
-        String desiredPage = "https://www.google.com/";
-
-        driverManager = WebDriverFactory.getManager(WebDriverType.OPERA);
-        driver = driverManager.getIPhoneElevenDriver();
-        driver.get(desiredPage);
-
-        if (driver.getCurrentUrl().equals(desiredPage))
-            System.out.println(">>> Opera Driver Functioning <<<");
-        //driverManager.quitDriver();
-    }
-
-    public static void safariTest(){
-        WebDriverManager driverManager;
-        WebDriver driver;
-        String desiredPage = "https://www.automationpractice.com/";
-
-        driverManager = WebDriverFactory.getManager(WebDriverType.SAFARI);
-        driver = driverManager.getIPhoneElevenDriver();
-        driver.get(desiredPage);
-
-        if (driver.getCurrentUrl().equals(desiredPage))
-            System.out.println(">>> Safari Driver Functioning <<<");
-        //driverManager.quitDriver();
+            System.out.println(">>> " + webDriverType + " Driver Functioning <<<");
+        driverManager.quitDriver();
     }
 
     public static void main(String[] args) {
-//        operaTest();
-//        chromeTest();
-//        firefoxTest();
-        //ieTest();
-        safariTest();
+        testDriver(WebDriverType.CHROME);
+        testHeadlessDriver(WebDriverType.OPERA);
+        testDriverMobile(WebDriverType.FIREFOX,MobilePhoneType.IPHONE11);
     }
 }
